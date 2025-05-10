7ff84f88f121c00c70f0f2304faac236eb4ad580
firebadnofire <william@archuser.org>
2025-05-10 17:40:48 -0400

    progress

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # secure-dns-proxy
 
+`secure-dns-proxy` binary that proxies DNS
+
+`upstreams-example.conf` conf to copy to etc/upstreams.conf with changes
+
+[x] Standard DNS support
+[x] DNS over HTTPS (DoH) support 
+[x] DNS over TLS (DoT) support
+[x] DNS over QUIC (DoQ) support
+
+Build instructions:
+
+```
+git clone https://codeberg.org/firebadnofire/secure-dns-proxy
+cd secure-dns-proxy
+CHANGE ME LATER YOU LAZY BUM
+```
+
+# Notes
+
+This project is licensed under the [GNU Affero General Public License](https://www.gnu.org/licenses/agpl-3.0.en.html). [Know your rights](https://choosealicense.com/licenses/agpl-3.0/).
+
+There is a `--insecure` flag you can use at runtime (eg. `sudo ./secure-dns-proxy --insecure`) that will disable TLS certificate verification. You should NEVER need to use this unless you are testing something, in which you should know very well what you are about to do.
--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"crypto/tls"
+	"flag"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -15,7 +20,10 @@ const (
 	relativeUpstreamConfPath = "../etc/secure-dns-proxy/upstreams.conf"
 )
 
-var upstreams []string
+var (
+	upstreams []string
+	insecure  bool
+)
 
 func getExecutableDir() string {
 	exePath, err := os.Executable()
@@ -40,9 +48,8 @@ func loadUpstreams(relPath string) []string {
 		if line == "" || strings.HasPrefix(line, "#") {
 			continue
 		}
-		if strings.HasPrefix(line, "dns://") {
-			address := strings.TrimPrefix(line, "dns://")
-			result = append(result, address)
+		if strings.HasPrefix(line, "dns://") || strings.HasPrefix(line, "https://") || strings.HasPrefix(line, "tls://") {
+			result = append(result, line)
 		} else {
 			log.Printf("[WARN] Skipping unsupported line: %s", line)
 		}
@@ -53,6 +60,61 @@ func loadUpstreams(relPath string) []string {
 	return result
 }
 
+func forwardDNSOverHTTPS(upstream string, msg *dns.Msg) (*dns.Msg, error) {
+	raw, err := msg.Pack()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", upstream, bytes.NewReader(raw))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/dns-message")
+	req.Header.Set("Accept", "application/dns-message")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	response := new(dns.Msg)
+	if err := response.Unpack(respBody); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func forwardDNSOverTLS(upstream string, msg *dns.Msg) (*dns.Msg, error) {
+	host := strings.TrimPrefix(upstream, "tls://")
+	if !strings.Contains(host, ":") {
+		host += ":853"
+	}
+
+	tlsConfig := &tls.Config{
+		ServerName:         strings.Split(host, ":")[0],
+		InsecureSkipVerify: insecure,
+	}
+	conn, err := tls.Dial("tcp", host, tlsConfig)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	dnsConn := &dns.Conn{Conn: conn}
+	if err := dnsConn.WriteMsg(msg); err != nil {
+		return nil, err
+	}
+	return dnsConn.ReadMsg()
+}
+
 func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
 	if len(upstreams) == 0 {
 		log.Print("[ERROR] No DNS upstreams available")
@@ -60,19 +122,37 @@ func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
 		return
 	}
 
-	c := new(dns.Client)
-	resp, _, err := c.Exchange(r, upstreams[0])
+	var resp *dns.Msg
+	var err error
+
+	upstream := upstreams[0] // Basic fallback for now
+	if strings.HasPrefix(upstream, "dns://") {
+		c := new(dns.Client)
+		address := strings.TrimPrefix(upstream, "dns://")
+		resp, _, err = c.Exchange(r, address)
+	} else if strings.HasPrefix(upstream, "https://") {
+		resp, err = forwardDNSOverHTTPS(upstream, r)
+	} else if strings.HasPrefix(upstream, "tls://") {
+		resp, err = forwardDNSOverTLS(upstream, r)
+	} else {
+		err = io.ErrUnexpectedEOF // crude placeholder
+	}
+
 	if err != nil {
 		log.Printf("[ERROR] DNS forward failed: %v", err)
 		dns.HandleFailed(w, r)
 		return
 	}
+
 	if err := w.WriteMsg(resp); err != nil {
 		log.Printf("[ERROR] Failed to write DNS response: %v", err)
 	}
 }
 
 func main() {
+	flag.BoolVar(&insecure, "insecure", false, "Skip TLS certificate verification")
+	flag.Parse()
+
 	upstreams = loadUpstreams(relativeUpstreamConfPath)
 	dns.HandleFunc(".", handleDNSRequest)
 
